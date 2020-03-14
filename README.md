2881cfa468bfeb816ede93ef8f272455cf10367e
didin jamaludin <didinj@gmail.com>
2020-03-15 06:36:20 +0700

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# React.js Tutorial: Facebook Login Example
+
+This source code is part of [React.js Tutorial: Facebook Login Example]().
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import { Card, Image } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState('');
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <Card style={{ width: '600px' }}>
+        <Card.Header>
+          { !login && 
+            <FacebookLogin
+              appId="562118384400275"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={responseFacebook}
+              icon="fa-facebook" />
+          }
+          { login &&
+            <Image src={picture} roundedCircle />
+          }
+        </Card.Header>
+        { login &&
+          <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>
+              {data.email}
+            </Card.Text>
+          </Card.Body>
+        }
+      </Card>
     </div>
   );
 }
